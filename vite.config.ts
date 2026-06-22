3b6836f56ab8de9b5107bfdd524e6b554157ecdd
Tomiwa Babatunde <tommybab7@gmail.com>
2026-06-22 05:13:23 -0700

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,6 @@ export default defineConfig({
       prerender: {
         enabled: true,
         crawlLinks: false,
-        renderTimeout: 10000,
       },
       pages: [
         { path: "/" },
